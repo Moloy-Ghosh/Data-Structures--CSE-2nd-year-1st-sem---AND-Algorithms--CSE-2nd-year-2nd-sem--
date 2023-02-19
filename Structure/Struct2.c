bc77aab1c8897889e37f67d12ae963c7bab4b649
Moloy Kumar Ghosh <118275263+Moloy-Ghosh@users.noreply.github.com>
2023-02-19 12:28:34 +0600

    Add files via upload

---
+++ Structure/Struct2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct birthday
+{
+    int month;
+    int day;
+    int year;
+};
+struct parents
+{
+    char name[20];
+    int age;
+};
+struct newBorn
+{
+    char name[20];
+    char sex[1];
+    struct birthday Bdy;
+    struct parents Father,Mother;
+};
+int main()
+{
+    struct newBorn baby1;
+    strcpy(baby1.name,"Akib");
+    strcpy(baby1.sex,"M");
+    baby1.Bdy.day=19;
+    baby1.Bdy.month=2;
+    baby1.Bdy.year=2023;
+    strcpy(baby1.Father.name,"Abdullah");
+    baby1.Father.age=37;
+    strcpy(baby1.Mother.name,"Sabmam");
+    baby1.Mother.age=33;
+
+    return 0;
+}
